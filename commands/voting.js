4dcb62ec89200be6c5b0333359370fc5269d9556
Filmos <78136833+Filmos@users.noreply.github.com>
2021-09-02 21:14:07 +0000

    Added basic database interaction

--- commands/voting.js
+++ commands/voting.js
@@ -1,5 +1,14 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
+const Client = new (require("@replit/database"))();
+
+async function addToMemory(interaction, count) {
+  let dbId = interaction.message.id
+  let cur = ((await Client.get(dbId))||0)*1+count
+  await Client.set(dbId, cur)
+  console.log(cur)
+  return "Votes: "+cur
+}
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -14,7 +23,8 @@ module.exports = {
       .setColor('#181a43')
       .setAuthor('VOTING', 'https://media.discordapp.net/attachments/557227661131251733/882377586204885043/voting.png')
       .setTitle(interaction.options.getString('proposal'))
-      .setDescription('3 days left\n0/1 votes casted')
+      .setDescription('For: 0\nAgainst: 0\nAbstain: 0')
+      .setFooter('3 days left\n0/1 votes casted')
 	    .setImage('http://filmos.net/assets/images/FilmosMed.png')
     
     const row = new MessageActionRow()
@@ -39,8 +49,8 @@ module.exports = {
     return interaction.reply({ content: `New voting created!`, ephemeral: true });
   },
   buttons: {
-    "for": (interaction => {return interaction.update({ content: 'For!' });}),
-    "abstain": (interaction => {return interaction.update({ content: 'Abstain!' });}),
-    "against": (interaction => {return interaction.update({ content: 'Against!' });})
+    "for": (async int => {return int.update({ content: await addToMemory(int, 1) });}),
+    "abstain": (async int => {return int.update({ content: await addToMemory(int, 0) });}),
+    "against": (async int => {return int.update({ content: await addToMemory(int, -1) });})
   }
 };
