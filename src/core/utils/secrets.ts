692026cf9e1536365d2b92aef975403122bfc0bd
Filmos <filmosyt@gmail.com>
2022-12-11 18:06:46 +0100

    Made building system-independent, removed type dependncies for secrets and added seperate test client

--- src/core/utils/secrets.ts
+++ src/core/utils/secrets.ts
@@ -2,7 +2,7 @@ import secrets from "../../secrets";
 
 export class Secrets {
     public static get(key: SecretsKey): string {
-        return secrets[key];
+        return secrets[key as keyof typeof secrets];
     }
 }
-export type SecretsKey = keyof typeof secrets
+export type SecretsKey = string
