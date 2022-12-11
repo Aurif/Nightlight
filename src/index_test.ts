0d226536f26ec320037eabc6a1278405bbd246a9
Filmos <filmosyt@gmail.com>
2022-12-11 18:06:46 +0100

    Made building system-independent, removed type dependncies for secrets and added seperate test client

--- src/index_test.ts
+++ src/index_test.ts
@@ -4,7 +4,7 @@ import { OpenAIEnvContext, OpenAIModule } from "./openai/module";
 import ChatBotScenario from "./scenarios/chatbot";
 
 worker()
-  .using<DiscordEnvContext>(new DiscordGuildModule({guildId: "888801480709128282", tokenKey: "discordBotToken"}))
+  .using<DiscordEnvContext>(new DiscordGuildModule({guildId: "888801480709128282", tokenKey: "discordTestBotToken"}))
   .using<OpenAIEnvContext>(new OpenAIModule({tokenKey: "openAIApiToken", textModels: {high: "text-davinci-002", medium: "text-curie-001", low: "text-babbage-001"}}))
   
   .run(new ChatBotScenario({channelId: ["888801481896128524", "888801579199770724"], maxPromptLength: 500, maxPreviousMessages: 7}))
