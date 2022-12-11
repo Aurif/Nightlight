a6a2103f5706ed4ecebf4d9bc45e2670b5b3d8f1
Filmos <filmosyt@gmail.com>
2022-12-11 18:34:30 +0100

    Chatbot - updated to newer version of davinci model

--- src/index_prod.ts
+++ src/index_prod.ts
@@ -7,7 +7,7 @@ import TheGateScenario from "./scenarios/the_gate";
 
 worker()
   .using<DiscordEnvContext>(new DiscordGuildModule({guildId: "812785611693490212", tokenKey: "discordBotToken"}))
-  .using<OpenAIEnvContext>(new OpenAIModule({tokenKey: "openAIApiToken", textModels: {high: "text-davinci-002", medium: "text-curie-001", low: "text-babbage-001"}}))
+  .using<OpenAIEnvContext>(new OpenAIModule({tokenKey: "openAIApiToken", textModels: {high: "text-davinci-003", medium: "text-curie-001", low: "text-babbage-001"}}))
   
   .run(new TheGateScenario({channelId: "945747669182210078", baseRoleId: "812786551242883143", additionalRoleIds: ["943935923035467826"]}))
   .run(new MessageMoverScenario({}))
--- src/index_test.ts
+++ src/index_test.ts
@@ -5,6 +5,6 @@ import ChatBotScenario from "./scenarios/chatbot";
 
 worker()
   .using<DiscordEnvContext>(new DiscordGuildModule({guildId: "888801480709128282", tokenKey: "discordTestBotToken"}))
-  .using<OpenAIEnvContext>(new OpenAIModule({tokenKey: "openAIApiToken", textModels: {high: "text-davinci-002", medium: "text-curie-001", low: "text-babbage-001"}}))
+  .using<OpenAIEnvContext>(new OpenAIModule({tokenKey: "openAIApiToken", textModels: {high: "text-davinci-003", medium: "text-curie-001", low: "text-babbage-001"}}))
   
   .run(new ChatBotScenario({channelId: ["888801481896128524", "888801579199770724"], maxPromptLength: 500, maxPreviousMessages: 7}))
